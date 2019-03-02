91c39ec8a227585b6b9b88bf358545e1f0523712
CoolONEOfficial <coolone.official@gmail.com>
2019-03-02 23:13:32 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
