ee6421004a8981762dbaeeafbf73de13ecb4b491
CoolONEOfficial <coolone.official@gmail.com>
2019-03-03 19:50:28 +0300

    Travis CI + Firebase integrated

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import Vuetify from 'vuetify'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueFire)
+firebase.initializeApp({
+  projectId: 'insurance-form',
+  databaseURL: 'insurance-form'
+})
+export const db = firebase.firestore()
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
